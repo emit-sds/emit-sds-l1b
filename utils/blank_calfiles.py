1f6ecec8bfa2a44dff74aa0f5e80f46f9b838d45
David R. Thompson <david.r.thompson@jpl.nasa.gov>
2021-01-09 13:10:11 -0800

    all cal files now present

--- utils/blank_calfiles.py
+++ utils/blank_calfiles.py
@@ -13,9 +13,11 @@ import json
 import logging
 import argparse
 from emitrdn import Config
+import random
+from numpy.random import normal
 
 
-header_string = """ENVI
+linearity_header_string = """ENVI
 description = {Linearity Correction File}
 samples = 65536
 lines = 1
@@ -25,6 +27,63 @@ file type = ENVI Standard
 data type = 4
 interleave = bsq
 byte order = 0"""
+
+bad_header_string = """ENVI
+description = {Bad Pixel Map}
+samples = {rows}
+lines = {cols}
+bands = 1
+header offset = 0
+file type = ENVI Standard
+data type = 4
+interleave = bsq
+byte order = 0"""
+
+dark_header_string = """ENVI
+description = {Dark Frame}
+samples = {rows}
+lines = {cols}
+bands = 1
+header offset = 0
+file type = ENVI Standard
+data type = 4
+interleave = bsq
+byte order = 0"""
+
+flat_header_string = """ENVI
+description = {Flat Field}
+samples = {rows}
+lines = {cols}
+bands = 1
+header offset = 0
+file type = ENVI Standard
+data type = 4
+interleave = bsq
+byte order = 0"""
+
+crf_header_string = """ENVI
+description = {CRF Correction}
+samples = {cols}
+lines = {cols}
+bands = 1
+header offset = 0
+file type = ENVI Standard
+data type = 4
+interleave = bsq
+byte order = 0"""
+
+srf_header_string = """ENVI
+description = {SRF Correction}
+samples = {rows}
+lines = {rows}
+bands = 1
+header offset = 0
+file type = ENVI Standard
+data type = 4
+interleave = bsq
+byte order = 0"""
+
+
 
 
 def main():
@@ -59,25 +118,55 @@ def main():
              sys.exit(1)
 
     # Make Radiometric Coefficients
-    n = 238
-    rows, cols = 328
+    rows, cols = 328, 1280
     dispersion = 7.4296517
     wl = 265+np.arange(328)*dispersion
-    fwhm = (wl[1]-wl[0])*1.1
-    rccs = np.array(n) * 0.0001
-    uncerts = np.array(n)
-    chn = np.arange(n)
+    fwhm = (wl[1] - wl[0]) * 1.1
+    rccs = np.array(rows) * 0.0001
+    uncerts = np.ones(rows) * 0.000001
+    chn = np.arange(rows)
+
     np.savetxt(np.c_[rccs, uncerts, chn], 
         config['radiometric_coefficient_file'])
+
     np.savetxt(np.c_[chn, wl,fwhm], 
         config['spectral_calibration_file'])
-    bad = np.
 
-    with open(args.output_file+'.hdr','w') as fout:
+    bad = np.zeros((rows,cols), dtype=np.float32)
+    nbad = 6
+    for n in nbad:
+        bad[random.randint(rows),random.randint(cols))] = -1
+    np.tofile(config['bad_element_file'], bad)
+    with open(config['bad_element_file']+'.hdr','w') as fout:
+        lcl = locals()
+        fout.write(bad_header_string.format(**lcl))
 
-
-
-    
+    dark_noise = 10
+    dark = np.array(normal(0, dark_noise, (rows,cols)), dtype=np.float32)
+    np.tofile(config['dark_frame_file'], dark)
+    with open(config['dark_frame_file']+'.hdr','w') as fout:
+        lcl = locals()
+        fout.write(dark_header_string.format(**lcl))
+        
+    srf = np.array(np.eye(rows), dtype=np.float32)
+    np.tofile(config['srf_correction_file'], dark)
+    with open(config['srf_correction_file']+'.hdr','w') as fout:
+        lcl = locals()
+        fout.write(srf_header_string.format(**lcl))
+        
+    crf = np.array(np.eye(rows), dtype=np.float32)
+    np.tofile(config['crf_correction_file'], dark)
+    with open(config['crf_correction_file']+'.hdr','w') as fout:
+        lcl = locals()
+        fout.write(crf_header_string.format(**lcl))
+ 
+    flat_noise = 0.01
+    flat = np.array(normal(1, flat_noise, (rows,cols)), dtype=np.float32)
+    np.tofile(config['flat_field_file'], flat)
+    with open(config['flat_field_file']+'.hdr','w') as fout:
+        lcl = locals()
+        fout.write(flat_header_string.format(**lcl))
+            
     count = sp.arange(2**16, dtype=sp.uint16).tofile(args['linearity_file'])
     with open(config['linearity_file'],'w') as fout:
         fout.write(header_string) 
