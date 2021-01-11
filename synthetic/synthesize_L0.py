26f1a4d97acfb49bee68750c96f2d979541038ba
David R. Thompson <david.r.thompson@jpl.nasa.gov>
2021-01-11 13:23:46 -0800

    update

--- synthetic/synthesize_L0.py
+++ synthetic/synthesize_L0.py
@@ -7,18 +7,17 @@
 
 import scipy.linalg
 import os, sys
-import scipy as sp
+import numpy as np
 from spectral.io import envi
 import json
 import logging
 import argparse
-from emitrdn import Config
 import random
 from numpy.random import normal
-
+from numpy.random import randint
 
 linearity_header_string = """ENVI
-description = {Linearity Correction File}
+description = {{Linearity Correction File}}
 samples = 65536
 lines = 1
 bands = 0
@@ -29,7 +28,7 @@ interleave = bsq
 byte order = 0"""
 
 bad_header_string = """ENVI
-description = {Bad Pixel Map}
+description = {{Bad Pixel Map}}
 samples = {rows}
 lines = {cols}
 bands = 1
@@ -40,7 +39,7 @@ interleave = bsq
 byte order = 0"""
 
 dark_header_string = """ENVI
-description = {Dark Frame}
+description = {{Dark Frame}}
 samples = {rows}
 lines = {cols}
 bands = 1
@@ -51,7 +50,7 @@ interleave = bsq
 byte order = 0"""
 
 flat_header_string = """ENVI
-description = {Flat Field}
+description = {{Flat Field}}
 samples = {rows}
 lines = {cols}
 bands = 1
@@ -62,7 +61,7 @@ interleave = bsq
 byte order = 0"""
 
 crf_header_string = """ENVI
-description = {CRF Correction}
+description = {{CRF Correction}}
 samples = {cols}
 lines = {cols}
 bands = 1
@@ -73,7 +72,7 @@ interleave = bsq
 byte order = 0"""
 
 srf_header_string = """ENVI
-description = {SRF Correction}
+description = {{SRF Correction}}
 samples = {rows}
 lines = {rows}
 bands = 1
@@ -94,7 +93,7 @@ def main():
     parser.add_argument('config_file')
     args = parser.parse_args()
 
-    with open(config_file,'r') as fin:
+    with open(args.config_file,'r') as fin:
         config = json.load(fin)
 
     for q in [config['radiometric_coefficient_file'],
@@ -105,7 +104,7 @@ def main():
               config['bad_element_file'],
               config['linearity_file'],
               config['dark_frame_file'],
-              config['output_rdn_file']]:
+              config['output_raw_file']]:
         if os.path.exists(q):
              print(q+' exists')
              sys.exit(1)
@@ -114,55 +113,53 @@ def main():
     rows, cols = 328, 1280
     dispersion = 7.4296517
     wl = 265+np.arange(328)*dispersion
-    fwhm = (wl[1] - wl[0]) * 1.1
-    rccs = np.array(rows) * 0.0001
+    fwhm = np.ones(rows) * (wl[1] - wl[0]) * 1.1
+    rccs = np.ones(rows) * 0.0001
     uncerts = np.ones(rows) * 0.000001
     chn = np.arange(rows)
 
-    np.savetxt(np.c_[rccs, uncerts, chn], 
-        config['radiometric_coefficient_file'])
+    np.savetxt(config['radiometric_coefficient_file'],np.c_[rccs, uncerts, chn])
 
-    np.savetxt(np.c_[chn, wl,fwhm], 
-        config['spectral_calibration_file'])
+    np.savetxt(config['spectral_calibration_file'],np.c_[chn, wl,fwhm])
 
     bad = np.zeros((rows,cols), dtype=np.float32)
     nbad = 6
-    for n in nbad:
-        bad[random.randint(rows),random.randint(cols))] = -1
-    np.tofile(config['bad_element_file'], bad)
+    for n in range(nbad):
+        bad[randint(rows),randint(cols)] = -1
+    bad.tofile(config['bad_element_file'])
     with open(config['bad_element_file']+'.hdr','w') as fout:
         lcl = locals()
         fout.write(bad_header_string.format(**lcl))
 
     dark_noise = 10
     dark = np.array(normal(0, dark_noise, (rows,cols)), dtype=np.float32)
-    np.tofile(config['dark_frame_file'], dark)
+    dark.tofile(config['dark_frame_file'])
     with open(config['dark_frame_file']+'.hdr','w') as fout:
         lcl = locals()
         fout.write(dark_header_string.format(**lcl))
         
     srf = np.array(np.eye(rows), dtype=np.float32)
-    np.tofile(config['srf_correction_file'], dark)
+    srf.tofile(config['srf_correction_file'])
     with open(config['srf_correction_file']+'.hdr','w') as fout:
         lcl = locals()
         fout.write(srf_header_string.format(**lcl))
         
     crf = np.array(np.eye(rows), dtype=np.float32)
-    np.tofile(config['crf_correction_file'], dark)
+    crf.tofile(config['crf_correction_file'])
     with open(config['crf_correction_file']+'.hdr','w') as fout:
         lcl = locals()
         fout.write(crf_header_string.format(**lcl))
  
     flat_noise = 0.01
     flat = np.array(normal(1, flat_noise, (rows,cols)), dtype=np.float32)
-    np.tofile(config['flat_field_file'], flat)
+    flat.tofile(config['flat_field_file'])
     with open(config['flat_field_file']+'.hdr','w') as fout:
         lcl = locals()
         fout.write(flat_header_string.format(**lcl))
             
-    count = sp.arange(2**16, dtype=sp.uint16).tofile(args['linearity_file'])
+    count = np.arange(2**16, dtype=np.uint16).tofile(config['linearity_file'])
     with open(config['linearity_file'],'w') as fout:
-        fout.write(header_string) 
+        fout.write(linearity_header_string) 
 
     obs_file = envi.open(args.obs_source_file+'.hdr')
     obs_metadata = obs_file.meta()
@@ -178,9 +175,9 @@ def main():
     raw_metadata['samples'] = cols
     envi.write_header(config['input_file']+'.hdr',raw_metadata,ext='',force=False)
     
-    with open(config['simulated_raw_file'],'wb') as rdn_out:
-      with open(config['simulated_obs_file'],'wb') as obs_out:
-        with open(config['simulated_loc_file'],'wb') as loc_out:
+    with open(config['output_raw_file'],'wb') as raw_out:
+      with open(config['output_obs_file'],'wb') as obs_out:
+        with open(config['output_loc_file'],'wb') as loc_out:
           with open(config['input_rdn_file'],'rb') as rdn_in:
             with open(config['input_obs_file'],'rb') as obs_in:
               with open(config['input_loc_file'],'rb') as loc_in:
@@ -197,7 +194,10 @@ def main():
                 rdn_valid = rdn_resamp[:,valid:]
                 desired_shape = np.array((rows, cols))
                 pads = tuple((0, i) for i in (desired_shape-rdn_valid.shape))
-                rdn_out = np.pad(rdn_valid, pads, mode="wrap")
+                raw = np.pad(rdn_valid, pads, mode="wrap")
+                raw = np.array(raw/rccs + dark, dtype=np.int16)
+                raw.tofile(raw_out)
+
 
                 obs = np.fromfile(obs_in, count=in_samples*in_obs, dtype=np.float32)
                 obs = obs.reshape((in_samples, in_obs))
