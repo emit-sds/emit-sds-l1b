84b008840540a7780a4599c5b6e996d16726fce0
pgbrodrick <phil.brodrick@gmail.com>
2021-11-04 18:34:29 -0700

    finish intial layout of netcdf data addtion, ummg still to add

--- output_conversion.py
+++ output_conversion.py
@@ -7,7 +7,7 @@ Authors: Philip G. Brodrick, philip.brodrick@jpl.nasa.gov
 
 import argparse
 from netCDF4 import Dataset
-from emit_utils.daac_converter import make2dVar, makeVectorVar, makeDims, make3dVar, makeGroups, makeGlobalAttr
+from emit_utils.daac_converter import add_variable, makeDims, makeGlobalAttr, add_loc, add_glt
 from emit_utils.file_checks import netcdf_ext, envi_header
 from spectral.io import envi
 import os
@@ -18,13 +18,12 @@ def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description='''This script \
     converts L1B PGE outputs to DAAC compatable formats, with supporting metadata''', add_help=True)
 
+    parser.add_argument('output_filename', type=str, help="Output netcdf filename")
     parser.add_argument('rdn_file', type=str, help="EMIT L1B radiance ENVI file")
     parser.add_argument('obs_file', type=str, help="EMIT L1B observation data ENVI file")
     parser.add_argument('loc_file', type=str, help="EMIT L1B location data ENVI file")
     parser.add_argument('glt_file', type=str, help="EMIT L1B glt ENVI file")
-    parser.add_argument('output_base', type=str, help="Output base for radiance files")
-    parser.add_argument('start_datetime', type=str, help="datetime string of acquisition start in YYYMMDDHHMMSS")
-    parser.add_argument('version', type=str, help="version number of processing")
+    parser.add_argument('--ummg_file', type=str, help="Output UMMG filename")
     parser.add_argument('--log_file', type=str, dfault=None, help="Logging file to write to")
     parser.add_argument('--log_level', type=str, default="INFO", help="Logging level")
     args = parser.parse_args()
@@ -40,8 +39,8 @@ def main():
     glt_ds = envi.open(envi_header(args.glt_file))
 
     # make the netCDF4 file
-    logging.info(f'Creating netCDF4 file: {netcdf_ext(args.output_base)}')
-    nc_ds = Dataset(netcdf_ext(args.output_base), 'w', clobber=True, format='NETCDF4')
+    logging.info(f'Creating netCDF4 file: {args.output_filename}')
+    nc_ds = Dataset(args.output_filename, 'w', clobber=True, format='NETCDF4')
 
     # make global attributes
     logging.debug('Creating global attributes')
@@ -56,84 +55,42 @@ def main():
         associated geometric information (path length, view angle, solar angle).\\n\\nThe calibrated at-sensor \
         radiance file contains radiance for each of {rdn_ds.shape} channels in units of microwatts per centimeter per \
         centimeter squared per steradian."
+    nc_ds.sync()
 
     logging.debug('Creating dimensions')
     makeDims(nc_ds, args.rdn_file, args.glt_file)
 
-    logging.debug('Creating groups')
-    makeGroups(nc_ds, ['radiance_data'])
+    logging.debug('Creating and writing radiance metadata')
+    add_variable(nc_ds, "sensor_band_parameters/radiance_wl", "f4", "Wavelength Centers", "nm",
+                 [float(d) for d in rdn_ds.metadata['wavelengths']], {"dimensions": ("number_of_bands",)})
 
-    ## Variables (bands \ actual data)
-    # instrument stuff
-    logging.debug('Creating and writing sensor_band_parameters group')
-    wvs = [float(d) for d in dsL1.hdr['rdn_clip']['wavelength'][0].split(',')]
-    makeVectorVar(nc_ds, '/sensor_band_parameters', '/wavelength', "f4", 'number_of_bands', 'Band center wavelengths',
-                  'nm', wvs)
+    add_variable(nc_ds, "sensor_band_parameters/radiance_fwhm", "f4", "Full Width at Half Max", "nm",
+                 [float(d) for d in rdn_ds.metadata['fwhm']], {"dimensions": ("number_of_bands",)})
 
-    fwhm = [float(d) for d in dsL1.hdr['rdn_clip']['fwhm'][0].split(',')]
-    makeVectorVar(nc_ds, '/sensor_band_parameters', '/fwhm', "f4", 'number_of_bands', 'full width at half max', 'nm',
-                  fwhm)
+    #add_variable(nc_ds, "scan_line_attributes", "scan_start_time", "f8", "Scan start time (UTC)", "seconds",
+    #             scan_start_times, {"dimensions": ("number_of_scans",)})
+    #add_variable(nc_ds, "scan_line_attributes", "scan_end_time", "f8", "Scan start time (UTC)", "seconds",
+    #             scan_end_times, {"dimensions": ("number_of_scans",)})
 
-    # Scan time (need to have a deeper look)
-    print('Creating and writing scan_line_attributes')
-    # this grabs the time stamp from the observation file
-    # pdb.set_trace()
-    makeVectorVar(nc_ds, '/scan_line_attributes', '/scan_start_time', "f8", 'number_of_scans', 'Scan start time (UTC)',
-                  'seconds', dsL1.data['scan_start_time'])
+    #TODO: consider adding geotransform variable to each variable
+    logging.debug('Creating and writing location data')
+    add_loc(nc_ds, args.loc_file)
+    logging.debug('Creating and writing glt data')
+    add_glt(nc_ds, args.glt_file)
 
-    # GLT file
-    ### In principle we can add attributes to the group instead of the variables.. e.g., map info
-    # Maybe we'll do that later. for now I'm sticking to the dirty fix solution
-    print('Creating and writing GLT Lookup')
-    # pdb.set_trace()
-    minfo = dsL1.hdr['rdn_glt']['map info']
-    make2dVar(nc_ds, '/glt_data', '/Sample', "i4", 'glt_y', 'glt_x', 'GLT sample Lookup', \
-              'pixel location', dsL1.data['glt sample lookup'], minfo)
-    make2dVar(nc_ds, '/glt_data', '/Line', "i4", 'glt_y', 'glt_x', 'GLT line Lookup', \
-              'pixel location', dsL1.data['glt line lookup'], minfo)
+    logging.debug('Creating and writing obs data')
+    nc_ds.createDimension('observation_bands', int(obs_ds.metadata['bands']))
+    add_variable(nc_ds, "obs", "d", "Observation Data", None,
+                 obs_ds.open_memmap(interleave='bip')[...].copy(), {"dimensions": ("ortho_y", "ortho_x", "observation_bands")})
 
-    # loc file
-    print('Creating and writing loc_data')
-    make2dVar(nc_ds, '/loc_data', '/longitude', "d", 'number_of_scans', 'pixels_per_scan',
-              'Longitudes of pixel locations', 'degrees_east', dsL1.data['longitude (wgs-84)'])
-    make2dVar(nc_ds, '/loc_data', '/latitude', "d", 'number_of_scans', 'pixels_per_scan',
-              'Latitudes of pixel locations', 'degrees_north', dsL1.data['latitude (wgs-84)'])
-    make2dVar(nc_ds, '/loc_data', '/Elevation', "d", 'number_of_scans', 'pixels_per_scan',
-              'Terrain elevation at pixel locations', 'meters', dsL1.data['elevation (m)'])
-    # OBS file
-    print('Creating and writing obs_data')
-    make2dVar(nc_ds, '/obs_data', '/path_length', "d", 'number_of_scans', 'pixels_per_scan',
-              'Distance between pixel and sensor', 'meters', dsL1.data['path length (m)'])
-    make2dVar(nc_ds, '/obs_data', '/sensor_zenith', "d", 'number_of_scans', 'pixels_per_scan', 'To-sensor zenith',
-              'degrees', dsL1.data['to-sensor zenith (0 to 90 degrees from zenith)'])
-    make2dVar(nc_ds, '/obs_data', '/to-sensor_azimuth', "d", 'number_of_scans', 'pixels_per_scan', 'To-sensor azimuth',
-              'degrees', dsL1.data['to-sensor azimuth (0 to 360 degrees cw from n)'])
-    make2dVar(nc_ds, '/obs_data', '/solar_zenith', "d", 'number_of_scans', 'pixels_per_scan',
-              'Solar zenith angle at pixel locations', 'degrees',
-              dsL1.data['to-sun zenith (0 to 90 degrees from zenith)'])
-    make2dVar(nc_ds, '/obs_data', '/to-sun_azimuth', "d", 'number_of_scans', 'pixels_per_scan',
-              'Solar azimuth angle at pixel locations', 'degrees',
-              dsL1.data['to-sun azimuth (0 to 360 degrees cw from n)'])
-    make2dVar(nc_ds, '/obs_data', '/solar_phase', "d", 'number_of_scans', 'pixels_per_scan', 'Solar phase angle',
-              'degrees', dsL1.data['solar phase'])
-    make2dVar(nc_ds, '/obs_data', '/slope', "d", 'number_of_scans', 'pixels_per_scan', 'Slope of the surface',
-              'degrees', dsL1.data['slope'])
-    make2dVar(nc_ds, '/obs_data', '/aspect', "d", 'number_of_scans', 'pixels_per_scan', 'Aspect of the surface',
-              'degrees', dsL1.data['aspect'])
-    make2dVar(nc_ds, '/obs_data', '/cosine_i', "d", 'number_of_scans', 'pixels_per_scan', 'cosine of SZA', 'unitless',
-              dsL1.data['cosine(i)'])
-    make2dVar(nc_ds, '/obs_data', '/UTC_Time', "d", 'number_of_scans', 'pixels_per_scan', 'Slope of the surface',
-              'Decimal Hours', dsL1.data['utc time'])
-    make2dVar(nc_ds, '/obs_data', '/Earth-sun-distance', "d", 'number_of_scans', 'pixels_per_scan',
-              'Distance between Earth and Sun', 'Astronomical Distance Units', dsL1.data['earth-sun distance (au)'])
-    # Radiance file
-    print('Creating and writing rdn_data')
-    make3dVar(nc_ds, '/rdn_data', '/rdn', "f4", 'number_of_bands', 'number_of_scans', 'pixels_per_scan',
-              'At-sensor Radiance', 'uW/cm/SR/nm', dsL1.data['rdn'])
+    add_variable(nc_ds, "sensor_band_parameters/observation_bands", str, "Observation Band Names", None,
+                 obs_ds.metadata['band names'], {"dimensions": ("observation_bands",)})
 
-    # Finished
+    add_variable(nc_ds, 'radiance', "f4", obs_ds.open_memmap(interleave='bip')[...].copy(), "Radiance Data",
+                 "uW/cm/SR/nm", {"dimensions":("number_of_bands", "number_of_scans", "pixels_per_scan")})
+    nc_ds.sync()
     nc_ds.close()
-    print('Successfully created', nc_fullpath)
+    logging.debug(f'Successfully created {args.output_filename}')
 
     return
 
