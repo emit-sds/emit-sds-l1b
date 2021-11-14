29ab4cb498bd48c0fae7b0dc904063e4ea941320
David R. Thompson <david.r.thompson@jpl.nasa.gov>
2021-11-13 22:33:52 -0800

    update

--- utils/makesrf.py
+++ utils/makesrf.py
@@ -7,6 +7,7 @@ from spectral.io import envi
 from scipy.stats import norm
 from scipy.linalg import solve, inv
 from astropy import modeling
+from sklearn.linear_model import RANSACRegressor
 import json
 
 
@@ -36,6 +37,7 @@ def main():
 
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('input')
+    parser.add_argument('--target_index',type=int,default=-1)
     args = parser.parse_args()
 
     infile = envi.open(find_header(args.input))
@@ -54,6 +56,7 @@ def main():
     columns = int(infile.metadata['samples'])
     lines = int(infile.metadata['lines'])
     nframe = rows * columns
+    margin=2
 
     allctrs,alllines = [],[]
     with open(args.input,'rb') as fin:
@@ -63,16 +66,24 @@ def main():
             # Read a frame of data
             frame = np.fromfile(fin, count=nframe, dtype=dtype)
             frame = np.array(frame.reshape((rows, columns)),dtype=np.float32)
-            maxind = np.argmax(np.sum(frame[:330,:],axis=0))
+            if args.target_index < 0:
+                maxind = np.argmax(np.sum(frame[:330,:],axis=0))
+            else:
+                maxind = args.target_index
             ctr,amplitude,std = find_peak(frame[:,maxind])
             fwhm = std * 2.0 * np.sqrt(2.0*np.log(2))
             allctrs.append(ctr)
             alllines.append(line)
-            print(line,ctr,fwhm)
+            #print(line,ctr,fwhm)
    
     chans_per_frame, offset = np.polyfit(alllines,allctrs,1)
+    robust_model = RANSACRegressor()
+    robust_model.fit(np.array(alllines)[:,np.newaxis],np.array(allctrs))
+    chans_per_frame_robust = robust_model.estimator_.coef_[0]
     chans_per_frame = abs(chans_per_frame)
+    chans_per_frame_robust = abs(chans_per_frame_robust)
     print('monochromator velocity:',chans_per_frame,'channels per frame')
+    print('robust monochromator velocity:',chans_per_frame_robust,'channels per frame')
 
     chans = np.unique([int(round(c)) for c in allctrs])
     sequences = {c:[] for c in chans}
@@ -84,14 +95,18 @@ def main():
             frame = np.fromfile(fin, count=nframe, dtype=dtype)
             frame = np.array(frame.reshape((rows, columns)),dtype=np.float32)
             maxind = np.argmax(np.sum(frame[:330,:],axis=0))
+            if args.target_index < 0:
+                maxind = np.argmax(np.sum(frame[:330,:],axis=0))
+            else:
+                maxind = args.target_index
             for ctr in chans:
                 sequences[ctr].append(frame[ctr,maxind])
         
     for ctr, sequence in sequences.items():
          c,amp,std = find_peak(sequence)
          fwhm = std * 2.0 * np.sqrt(2.0*np.log(2))
-         fwhm = fwhm * chans_per_frame
-         print(ctr,std * )
+         fwhm = fwhm * chans_per_frame_robust
+         print(ctr,fwhm)
     print('done') 
 
 if __name__ == '__main__':
