a50fae3f29396233d33f77d8dfe52d30d153d06e
David R. Thompson <david.r.thompson@jpl.nasa.gov>
2022-01-17 13:37:32 -0800

    new bad pixel script

--- scripts/step1_make_badpixels.sh
+++
@@ -1,5 +0,0 @@
-# David R Thompson
-
-# Make bad pixels
-python  ../utils/makebad.py /beegfs/scratch/drt/20211114_EMIT_Radcal/20211116_200400_UTC_GenericFOV/20211116_200836_UTC_GenericFOV_Fields-250-1455_darksub_pedestal ../data/EMIT_Bad_Elements_20211229
-
