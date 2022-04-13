d1e10a99b608441e1fe903c67b91665d21409cab
David R. Thompson <david.r.thompson@jpl.nasa.gov>
2022-04-13 10:35:55 -0700

    new flatfield approach

--- scripts/cwis_step10_flatfield.py
+++ scripts/cwis_step10_flatfield.py
@@ -1,21 +1,26 @@
 # David R Thompson
 import os
 
-# Intensity-based selection method
-if False:
-    infile = '/beegfs/scratch/drt/20211130_EMIT_RadCal/20211116_200400_UTC_GenericFOV/20211116_200836_UTC_GenericFOV_Fields-250-1455_clip_darksub_pedestal_badfix_osffix_linear_scatterfix_ghostfix'
-    backgroundfile = '/beegfs/scratch/drt/20211130_EMIT_RadCal/20211116_203242_UTC_GenericFOV/20211116_203750_UTC_GenericFOV_Fields-250-1455_clip_darksub_pedestal_badfix_osffix_linear_scatterfix_ghostfix'
-    outfile = '../data/EMIT_FlatField_20220117'
-    cmd = 'python ../utils/makeflat.py --background %s %s %s' %(backgroundfile, infile, outfile)
-    print(cmd)
-    os.system(cmd)
-
 # Manual selection method using a mask image
-if True:
-    infile = '/beegfs/scratch/drt/20211130_EMIT_RadCal/20211116_200400_UTC_GenericFOV/20211116_200836_UTC_GenericFOV_Fields-250-1455_clip_darksub_pedestal_badfix_osffix_linear_scatterfix_ghostfix'
-    backgroundfile = '/beegfs/scratch/drt/20211130_EMIT_RadCal/20211116_203242_UTC_GenericFOV/20211116_203750_UTC_GenericFOV_Fields-250-1455_clip_darksub_pedestal_badfix_osffix_linear_scatterfix_ghostfix'
-    outfile = '../data/EMIT_FlatField_20220117'
-    cmd = 'python ../utils/makeflat.py --mask_image ../data/emit_radcal_mask.png --background %s %s %s' %(backgroundfile, infile, outfile)
+if False:
+    infile = '/beegfs/scratch/drt/20220112_CWIS2/20220107_flatfield/light_80pc_darksub_pedestal_ghostfix'
+    mask = '/beegfs/scratch/drt/20220112_CWIS2/20220107_flatfield/light_80pc_mask.png'
+    #backgroundfile = '/beegfs/scratch/drt/20211130_EMIT_RadCal/20211116_203242_UTC_GenericFOV/20211116_203750_UTC_GenericFOV_Fields-250-1455_clip_darksub_pedestal_badfix_osffix_linear_scatterfix_ghostfix'
+    outfile = '../data/CWIS_FlatField_20220218'
+    cmd = 'python ../utils/makeflat.py --mask_image %s %s %s' %(mask, infile, outfile)
     print(cmd)
     os.system(cmd)
 
+    infile = '/beegfs/scratch/drt/20220112_CWIS2/20220107_flatfield/light_40pc_darksub_pedestal_ghostfix'
+    mask = '/beegfs/scratch/drt/20220112_CWIS2/20220107_flatfield/light_40pc_mask.png'
+    #backgroundfile = '/beegfs/scratch/drt/20211130_EMIT_RadCal/20211116_203242_UTC_GenericFOV/20211116_203750_UTC_GenericFOV_Fields-250-1455_clip_darksub_pedestal_badfix_osffix_linear_scatterfix_ghostfix'
+    outfile = '../data/CWIS_FlatField_20220218a'
+    cmd = 'python ../utils/makeflat.py --mask_image %s %s %s' %(mask, infile, outfile)
+    print(cmd)
+    os.system(cmd)
+
+infile = '/beegfs/scratch/drt/20220112_CWIS2/20220412_radiometry/20220412_Radiometric_Apo_Box_Scan_darksub_pedestal_ghostfix' 
+outfile = '../data/CWIS_FlatField_20220413'
+cmd = 'python ../utils/makeflat.py --config ../config/cwis2.json %s %s' %(infile, outfile)
+print(cmd)
+os.system(cmd)
