62a6ef29090ecc970364b179ca50b512249d21ca
David R. Thompson <davidraythompson@gmail.com>
2023-10-02 08:27:33 -0700

    new scripts and calibration results

---
+++ scripts/cpm/cpm_step05_wavelengths.py
@@ -0,0 +1,134 @@
+# David R Thompson
+import pylab as plt
+import numpy as np
+from scipy.stats import norm
+from spectral.io import envi
+from scipy.interpolate import interp1d
+import astropy.modeling as modeling
+from astropy.modeling.models import custom_model
+from astropy.modeling.fitting import LevMarLSQFitter
+from scipy.optimize import minimize
+from scipy.signal import medfilt
+from numpy.random import randn
+import json
+
+# Fit a gaussian to a single peak in an ordered series of data
+def find_peak(x, plot=False):
+    fitter = modeling.fitting.LevMarLSQFitter()
+    model = modeling.models.Gaussian1D(amplitude=np.max(x),
+                                       mean=np.argmax(x),
+                                       stddev=1.0/2.35)   # depending on the data you need to give some initial values
+    fitted_model = fitter(model, np.arange(len(x)), x)
+    if plot:
+         print(fitted_model.mean[0], fitted_model.amplitude[0], fitted_model.stddev[0])
+         plt.plot(x)
+         plt.plot(fitted_model(np.arange(len(x))))
+         plt.show()
+    return fitted_model.mean[0], fitted_model.amplitude[0], fitted_model.stddev[0]
+
+
+# We will analyze the laser sphere data from TVAC2.  It has already
+# been clipped to the standard subframe, dark-subtracted, and 
+# pedestal-shift corrected
+basedir = '/beegfs/scratch/drt/20231001_CPM_Wavelengths/20230904_110831_UTC_LaS/'
+I = envi.open(basedir+'/20230904_110919_UTC_LaS_Fields-0-639_darksub_pedestal.hdr')
+I = I.load()
+
+# Laser info from 20211112_211905_UTC_LaS_Fields-40-1319
+# Wavelengths from Christine.  We start with an initial first guess of the channels
+# based on her mesurements.
+wavelengths = np.array([1949.10,1064.38,532.19])  # According to PBAT sources are 532.209 nm, 1064.324 nm, 1949.88 nm
+sampling = 5.0
+uncert = sampling *  np.array([0.073, 0.007, 0.026, 0.079, 0.077])
+channels = np.array([140,316,422])
+nlasers = len(channels)
+
+# Change to refractive wavelength of vacuum
+index_of_refraction = np.array([1.000268,1.000269,1.000273])
+wavelengths = wavelengths * index_of_refraction
+ 
+# These channels are reported in whole-FPA format, but the EMIT FPA begins reading at row 7
+nrows, ncols = 480,640
+
+margin = 5
+
+# our list of laser fits, one sublist per laser
+observed = [[] for c in channels]
+
+# Find a the spatial location of the lasers, and fit each laser peak
+# put them as column, row, wavelength triplets into the "observed" list
+for line in range(I.shape[0]):
+   frame = np.squeeze(I[line,:,:]).T 
+   col,amp,_ = find_peak(medfilt(frame.mean(axis=0),5))
+   print(col,amp)
+   if amp<100:
+       continue
+   for i, w, chn in zip(range(nlasers), wavelengths, channels):
+       idx = np.arange(int(chn-margin),int(chn+margin+1), dtype=int)
+       row,_,_ = find_peak(frame[idx,int(round(col))])
+       row = row+idx[0] 
+       observed[i].append([col,row,w])
+
+# Now plot the result, and save the laser fits to a file for later plotting
+x = np.zeros((nlasers,ncols))
+y = np.zeros((nlasers,ncols))
+colors=['r','b','k']
+for i in range(len(observed)):
+    D = np.array(observed[i])
+    # map row to column for a single laser
+    p = np.polyfit(D[:,0],D[:,1],1)
+    x[i,:] = np.polyval(p,np.arange(ncols))
+    y[i,:] = wavelengths[i]
+    offset = np.mean(D[:,1])
+    plt.plot(D[:,0],D[:,1]-offset,colors[i]+'.')
+    plt.plot(np.arange(ncols),x[i,:]-offset,colors[i])
+    np.savetxt('../../data/plots/CPM_Laser_%i_ColRow.txt'%wavelengths[i],D,fmt='%8.6f')
+plt.show()
+
+# now we fit the nonuniform dispersion curve
+# we assume it is similar to the EMIT Dyson dispersion curve, and translate
+average = 7.431625
+EMIT_CPM_Translation = 5.0/average
+model_wavelengths=np.array([380.00000,439.00000,498.00000,557.00000,616.00000,675.00000,800.00000,925.00000,1050.00000,1175.00000,1300.00000,1542.00000,1784.00000,2026.00000,2268.00000,2510.00000])
+model_dispersions=np.array([7.39600,7.41900,7.43300,7.44200,7.44800,7.45200,7.45700,7.45800,7.45600,7.45300,7.45000,7.43900,7.42600,7.41100,7.39300,7.37300]) * EMIT_CPM_Translation
+
+# FPA is flipped
+model_dispersions = -np.array(model_dispersions)
+
+dispersion = interp1d(model_wavelengths, model_dispersions, bounds_error=False, fill_value='extrapolate')
+
+def gen_wavelengths(x, model_wavelengths=model_wavelengths, model_dispersions=model_dispersions):
+  start = x[0]
+  stretch = x[1]
+  wls = [start]
+  for i in range(1,nrows):
+     wls.append(wls[-1]+dispersion(wls[-1])*stretch)
+  return np.array(wls)
+
+def errs(x,actual_wavelengths,actual_columns):
+  wl = gen_wavelengths(x)
+  predicted_wl = interp1d(np.arange(len(wl)), wl, bounds_error=False, fill_value='extrapolate')(actual_columns)
+  err = np.sum((actual_wavelengths - predicted_wl)**2)
+  return err 
+
+
+ctrs = np.zeros((nrows, ncols, 2))
+
+# Perform the fit for each column
+ctrs = np.zeros((nrows, ncols, 2))
+for i in [int(ncols/2)]:#range(ncols):
+  print('column',i)
+  x0 = np.array([2640,1])
+  best = minimize(errs,x0,args=(y[:,i],x[:,i])) 
+  p = np.polyfit(x[:,i],y[:,i],1)
+  ctrs[:,i,0] = gen_wavelengths(best.x)
+  print(best.x)
+# save out wavelength center matrix
+envi.save_image('../../data/cpm/CPM_WavelengthCenters_20231001.hdr',np.array(ctrs,dtype=np.float32),ext='',force=True)
+
+# save out ascii text file, no FWHMs, in microns
+wvl = np.squeeze(ctrs[:,:,0]).mean(axis=1)
+wvl = ctrs[:,int(ncols/2),0]
+fwhm = np.zeros(nrows)
+chn = np.arange(480)
+np.savetxt('../../data/cpm/CPM_Wavelengths_20231001.txt',np.c_[chn,wvl/1000.0,fwhm], fmt='%10.8f')
---
+++ scripts/cpm/cpm_step06_radcal.py
@@ -0,0 +1,181 @@
+# David R Thompson
+import numpy as np
+import pylab as plt
+import os, sys
+from spectral.io import envi
+from scipy.interpolate import interp1d
+
+plot = True
+frame_averaging = 15
+
+q,wl,fwhm = np.loadtxt('../../data/cpm/CPM_Wavelengths_20231001.txt').T * 1000.0
+
+def resample(wl_old, spectrum, method='linear'):
+  p = interp1d(wl_old, spectrum, kind=method, fill_value='extrapolate', bounds_error=False)
+  return p(wl)
+
+# Window transmittance
+wl_window, window_trans = np.loadtxt('../../data/ogse/cpm/window_infrasil301-302_transmittance.txt',skiprows=1).T
+window_trans = resample(wl_window, window_trans)
+window_uncert = np.ones(len(window_trans)) * 0.01
+
+
+# -------------------------------------------------- NIST lamp and panel calibration
+
+# Load irradiance, translate uncertainty from percenmt to one sigma irradiance
+wl_irr, irr, irradiance_uncert = np.loadtxt('../../data/ogse/cpm/lamp_s1359_irradiance.txt', 
+    skiprows=1).T 
+irradiance = resample(wl_irr, irr, method='cubic')
+irradiance_uncert = resample(wl_irr, irradiance_uncert)
+irradiance_uncert = irradiance_uncert / 100.0 * irradiance
+
+# Spectralon reflectance
+wl_spec, spec_rfl, spec_uncert =\
+     np.loadtxt('../../data/ogse/cpm/SRT-99-120-7015-2665.txt',skiprows=1).T  
+spectralon_rfl = resample(wl_spec, spec_rfl)
+spectralon_uncert = resample(wl_spec, spec_uncert)
+
+# BRDF
+brdf_factor = np.ones(len(wl)) * 1.015
+brdf_uncert = np.ones(len(wl)) * 0.01
+
+# Radiance 
+rdn = irradiance * spectralon_rfl * window_trans / np.pi * brdf_factor 
+print('!',irradiance[10],spectralon_rfl[10],window_trans[10],brdf_factor[10])
+
+distance_uncert = 0.0015875 # meters
+distance = 0.5
+distance_uncert_rdn =( 1-(0.5**2)/((0.5+distance_uncert)**2)) * rdn
+
+# Derivatives of radiance
+drdn_dirr    =              spectralon_rfl * window_trans / np.pi * brdf_factor 
+drdn_dspec   = irradiance *                  window_trans / np.pi * brdf_factor 
+drdn_dtrans  = irradiance * spectralon_rfl                / np.pi * brdf_factor 
+drdn_dbrdf   = irradiance * spectralon_rfl * window_trans / np.pi                
+
+rdn_uncert = np.sqrt((drdn_dirr * irradiance_uncert)**2 + \
+                     (drdn_dspec * spectralon_uncert)**2 + \
+                     (drdn_dtrans * window_uncert)**2 + \
+                     (drdn_dbrdf * brdf_uncert)**2 + \
+                     (distance_uncert_rdn**2))
+
+
+basedir = '/beegfs/scratch/drt/20230928_CPM_Radcal/'
+
+# subtract background
+background = basedir+'20230904_193211_UTC_RADCAL/20230904_193416_UTC_RadCal_Fields-0-590_darksub_pedestal'
+foreground = basedir+'20230904_193731_UTC_RADCAL/20230904_193936_UTC_RadCal_Fields-0-590_darksub_pedestal'
+bgsub = basedir+'20230904_193731_UTC_RADCAL/20230904_193936_UTC_RadCal_Fields-0-590_darksub_pedestal_bgsub'
+if not os.path.exists(bgsub):
+  with open(background,'rb') as bg_fin:
+    with open(foreground,'rb') as fg_fin:
+      with open(bgsub,'wb') as fout:
+        for i in range(14997):
+          bg = np.fromfile(bg_fin, count=480*640, dtype=np.float32)
+          fg = np.fromfile(fg_fin, count=480*640, dtype=np.float32)
+          bgs = np.array(fg-bg,dtype=np.float32)
+          bgs.tofile(fout)
+
+# make header
+bghdr = bgsub+'.hdr'
+if not os.path.exists(bghdr):
+  cmd = 'cp %s %s'%(foreground+'.hdr',bghdr)
+  print(cmd)
+  os.system(cmd)
+
+# Run makeflat.py here
+config = '../../config/cpm.json'          
+flat = basedir+'20230904_193731_UTC_RADCAL/20230904_193936_UTC_RadCal_Fields-0-590_darksub_pedestal_bgsub_flat'
+if not os.path.exists(flat):
+  cmd = 'python ../../utils/makeflat.py --config %s %s %s'%(config,bgsub,flat)
+  print(cmd)
+  os.system(cmd)
+
+input_files = [flat]
+output_files = ['CPM_RadiometricCoeffs_8ms_20231002.txt']
+
+for input_file, outfile in zip(input_files, output_files):
+
+    I = envi.open(input_file+'.hdr')
+    DN = np.array([float(d) for d in I.metadata['average_dns']])
+    DN_std = np.array([float(d) for d in I.metadata['stdev_dns']])
+
+    channels = np.arange(len(wl),dtype=int)
+    factors = rdn / DN
+
+    plt.plot(wl,factors)
+
+    # Interpolate over water vapor absorption at 1.88 microns
+    a = np.argmin(abs(wl-1780))
+    b = np.argmin(abs(wl-1800))
+    c = np.argmin(abs(wl-1950))
+    d = np.argmin(abs(wl-1970))
+    edges = np.concatenate((np.arange(d,c), np.arange(b,a)), axis=0)
+    interior = np.arange(c,b)
+    channels = np.arange(len(factors))
+    model = np.polyfit(channels[edges],factors[edges],2)
+    factors[interior] = np.polyval(model, channels[interior])
+
+    # Interpolate over water vapor absorption at 1.38 microns
+    a = np.argmin(abs(wl-1335))
+    b = np.argmin(abs(wl-1345))
+    c = np.argmin(abs(wl-1420))
+    d = np.argmin(abs(wl-1430))
+    edges = np.concatenate((np.arange(d,c), np.arange(b,a)), axis=0)
+    interior = np.arange(c,b)
+    channels = np.arange(len(factors))
+    model = np.polyfit(channels[edges],factors[edges],2)
+    factors[interior] = np.polyval(model, channels[interior])
+
+    # Show the interpolated result
+    plt.semilogy(wl,factors)
+    plt.ylim([0,0.02])
+    plt.xlabel('Wavelength (nm)')
+    plt.ylabel('Radiometric Calibration Coefficients (uW nm-1 sr-1 cm-2 DN-1)')
+    plt.box(False)
+    plt.grid(True)
+    plt.xlim([380,2500])
+    plt.show()
+
+    factors_uncert = rdn_uncert / DN
+    SNR = DN/DN_std/np.sqrt(frame_averaging)
+
+
+    np.savetxt(outfile,np.c_[channels,factors,factors_uncert], fmt='%10.8f')
+
+    if plot:
+       plt.plot(wl,irradiance,color=[0.8,0.2,0.2])
+       plt.plot(wl_irr,irr,'ko')
+       plt.show()
+
+    if plot:
+       plt.figure(figsize=(9,9))
+       plt.plot(wl, drdn_dspec * spectralon_uncert / rdn, color=[0.8, 0.2, 0.2])
+       plt.plot(wl, drdn_dtrans * window_uncert / rdn, color=[0.2, 0.8, 0.2])
+       plt.plot(wl, drdn_dirr * irradiance_uncert / rdn, color=[0.2, 0.2, 0.8])
+       plt.plot(wl, drdn_dbrdf * brdf_uncert / rdn, color=[0.2, 0.8, 0.8])
+       plt.plot(wl, distance_uncert_rdn / rdn, color=[0.8, 0.2, 0.8])
+       plt.plot(wl, rdn_uncert/rdn, 'k--')
+       plt.legend(('Spectralon reflectance','Window transmittance',
+             'Lamp irradiance','Spectralon BRDF',
+             'OGSE geometry','Total uncertainty'))
+       plt.grid(True)
+       plt.box(False)
+       plt.xlabel('Wavelength (nm)')
+       plt.ylabel('Radiometric uncertainty, fractional')
+       plt.xlim([380,2500])
+       plt.ylim([0,0.1])
+    plt.show()
+
+cmd = 'cp ' 
+cmd = cmd+flat
+cmd = cmd + ' ' + '../../data/cpm/CPM_Flatfield_20231002'
+print(cmd)
+os.system(cmd)
+
+cmd = 'cp ' 
+cmd = cmd+flat+'.hdr'
+cmd = cmd + ' ' + '../../data/cpm/CPM_Flatfield_20231002.hdr'
+print(cmd)
+os.system(cmd)
+
